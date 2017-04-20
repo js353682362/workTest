d9461494e1dc8a371ed33991826b26121655cfa0
353682362 <353682362@qq.com>
2017-04-20 14:29:42 +0800

    创建

---
+++ src/main/java/com/jiaos/common/HttpRequestUtil.java
@@ -0,0 +1,28 @@
+package com.jiaos.common;
+
+import javax.servlet.ServletRequest;
+
+/**
+ * @（#）:HttpRequestUtils.java
+ * @description:
+ * @author: jiaosen 2017/4/20
+ * @version: Version 1.0
+ */
+public class HttpRequestUtil {
+
+    public static String getValue(ServletRequest request,String paramName){
+        return request.getParameter(paramName);
+    }
+
+    public static String getString(ServletRequest request,String paramName){
+        return getValue(request,paramName);
+    }
+
+    public static Long getLong(ServletRequest request,String paramName){
+        return Long.valueOf(getString(request,paramName));
+    }
+
+    public static Integer getInteger(ServletRequest request,String paramName){
+        return Integer.valueOf(getString(request,paramName));
+    }
+}
--- src/main/java/com/jiaos/controller/RedirectController.java
+++ src/main/java/com/jiaos/controller/RedirectController.java
@@ -1,10 +1,25 @@
 package com.jiaos.controller;
 
+import com.jiaos.common.HttpRequestUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.ServletRequest;
+
 /**
  * @（#）:RedirectController.java
  * @description:
  * @author: jiaosen 2017/4/20
  * @version: Version 1.0
  */
+@Controller
+@RequestMapping({"/common"})
 public class RedirectController {
+
+    @RequestMapping(value = "/toPage",method = RequestMethod.GET)
+    public String toPage(ServletRequest request){
+        return HttpRequestUtil.getValue(request,"toPage");
+    }
+
 }
