cfde879174bfa2b8d8f40c0666e63ef43b3fcbdc
353682362 <353682362@qq.com>
2017-04-20 14:15:30 +0800

    修改文件

---
+++ src/main/java/com/jiaos/controller/RedirectController.java
@@ -0,0 +1,10 @@
+package com.jiaos.controller;
+
+/**
+ * @（#）:RedirectController.java
+ * @description:
+ * @author: jiaosen 2017/4/20
+ * @version: Version 1.0
+ */
+public class RedirectController {
+}
